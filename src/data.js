f4345a2e758ac64a358ae7f72dbb13db09ff75e6
Emin Pogosbeko <pogosbekovemin@gmail.com>
2022-01-20 22:55:47 +0300

    Slider created

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: '/img/image1.jpg',
+    alt: 'image1',
+    title: 'Summer sale',
+    desc: 'Dont compromise on style! Get flat 30% off for new arrivals.',
+    bg: 'E8E9EB'
+  },
+  {
+    id: 2,
+    img: '/img/image2.jpg',
+    alt: 'image2',
+    title: 'Winter sale',
+    desc: 'Dont compromise on style! Get flat 30% off for new arrivals.',
+    bg: 'E5E6E8'
+  },
+  {
+    id: 3,
+    img: '/img/image3.jpg',
+    alt: 'image3',
+    title: 'No sale',
+    desc: 'Dont compromise on style! Get flat 30% off for new arrivals.',
+    bg: 'fbf0f4'
+  },
+]
